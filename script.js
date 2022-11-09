88703479440e0db255986111b6a60bcf8d5d50c6
Alexandr <s3mbl4@gmail.com>
2022-11-09 23:53:53 +0100

    09.11.22

--- script.js
+++ script.js
@@ -3,19 +3,17 @@
 // let result =  userName + surname;
 // alert(result)
 
-
 // let userName = prompt("Имя")
 
 // console.log ("Hello," + userName)
 
-// let isRaining = confirm("Сейчас идет дождь ?") 
+// let isRaining = confirm("Сейчас идет дождь ?")
 // if(isRaining == true){
 //     alert("Возьми зонт")
 // }
 // else if(isRaining == false){
 //     alert("Зонт не нужен")
 // }
-
 
 // let sale = 701
 // if(sale > 700){
@@ -40,28 +38,148 @@
 
 // ____________________________ДЗ №1______________________________________________
 
-let r = 20
-let g = 50
-let b = 200
-let sum = "rgb(" + r + "," + g + "," + b +")"
-console.log(sum)
+// let r = 20
+// let g = 50
+// let b = 200
+// let sum = "rgb(" + r + "," + g + "," + b +")"
+// console.log(sum)
 
-//_____________________________ДЗ №2______________________________________________
+// //_____________________________ДЗ №2______________________________________________
 
-let number = +prompt("Введите число от 0 до 200", 0)
-if(number > 100){
-    alert("Ваше число больше чем 100")
-}else if(number == 100){
-    alert("Вы ввели 100")
-}else{
-    alert("Ваше число меньше чем 100")
+// let numberOne = +prompt("Введите число от 0 до 200", 0);
+// let numberTwo = +prompt("Введите число от 0 до 200", 0);
+
+// if (numberOne > numberTwo) {
+//   alert(numberOne);
+// } else if (numberOne == numberTwo) {
+//   alert("Числа равны");
+// } else {
+//   alert(numberTwo);
+// }
+
+// console.log(typeof number);
+
+// // //_____________________________ДЗ №3_________________________________________________
+
+// // let a = +prompt("Введите первое любое число", 0)
+// // let b = +prompt("Введите второе любое число", 0)
+// // console.log("Сумма = " + (a + b), "Вычетание = " + (a - b), "Умножение = " + a * b, "Деление = " + a / b)
+
+// let book = {
+//   name : "JavaScript",
+//   pages : 250,
+//   price : 50,
+//   img : "https://JS.png",
+// };
+// console.log(book)
+
+// let books = ["Harry1" , "Harry2" , "Harry3"]
+// console.log(books)
+
+// let user = ["Alex", "29", "https://777.jpg", ["box", "JS", "Voleyball"] ]
+// console.log(user)
+
+// num.push(5)
+// console.log(num)
+// num.unshift(-5)
+// console.log(num)
+
+// let nums = [100, 500]
+// let numOne = +prompt("Введите число")
+// let numTwo = +prompt("Введите число")
+// let numThree= +prompt("Введите число")
+
+// nums.push(numOne, numTwo, numThree)
+// console.log(nums)
+
+// let nums = [100, 500, 400, 700 ,1000]
+//
+// for(let i = 0; i<nums.length; i++){
+//     console.log(nums[i])
+// }
+
+// let result = 5
+// for( let i = 0; i < 5; i++){
+//     console.log(i * result)
+// }
+
+// myName = "Alex"
+// for( let i = 0; i < 101; i++){
+//     if(i<30){
+//         console.log(i)
+//     }else if(i>29)
+//     console.log("Больше", i)
+// }
+
+// let result = 5
+// for( let i = 0; i < 5; i++){
+//     console.log(i * result)
+// }
+
+// let nums = [100, 500, 400, 700 ,1000]
+
+// for(let i = 0; i < nums.length; i++){
+//     console.log(nums[i])
+// }
+
+// let nums = [32, 51, 60, 78 ,105, 14, 10]
+// for(let i = 0; i < nums.length; ++i){
+//     if(nums[i] >= 50)
+//     console.log(nums[i])
+// }
+
+// ДЗ.1. Есть массив [-10, 50, 60, 0, -100, 125]. Вывести в консоль только + числа.
+// ДЗ.2. Есть массив [11, 12, 15, 17, 18, 25, 26]. Вывести в консоль только четные числа
+// ДЗ.3  Есть массив [10, 20, 30 ,40 , 50, 60]. Найти сумму чисел в массив и вывести их в консоль.
+// ДЗ.4. Есть массив [-10, 50, 60, 0, -100, 125]. Найти произведение отрицательных чисел
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//_______________________ДЗ №1__________________________________
+let arr = [-10, 50, 60, 0, -100, 125]
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 0)
+    console.log(arr[i])
 }
 
-console.log(typeof number)
+//______________________ДЗ №2___________________________________
+let arr = [11, 12, 15, 17, 18, 25, 26]
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 == 0)
+    console.log(arr[i])
+}
 
-//_____________________________ДЗ №3_________________________________________________
+//_____________________ДЗ №3_____________________________________
+//РУЧНОЙ РЕЖИМ
+let arr = [10, 20, 30, 40, 50, 60]
+let sum = arr["0"]+arr["1"]+arr["2"]+arr["3"]+arr["4"]+arr["5"]
+console.log(sum)
 
-let a = +prompt("Введите первое любое число", 0)
-let b = +prompt("Введите второе любое число", 0)
-console.log("Сумма = " + (a + b), "Вычетание = " + (a - b), "Умножение = " + a * b, "Деление = " + a / b)
+//АВТОМАТИЗИРОВАНЫЙ
+let arr = [10, 20, 30, 40, 50, 60]
+sum = 0
+for(let i = 0; i < arr.length; i++){
+    sum += arr[i]
+}
+console.log(sum)
 
+//___________________ДЗ №4_______________________________________
+
+let arr = [-10, 50, 60, 0, -100, 125];
+let mult = 1;
+for (let i = 0; i < arr.length; i++){
+  if (arr[i] < 0) {
+    mult *= arr[i];
+  }
+ }
+console.log(mult);
+
+/* ДЗ.5 Запросить 10 чисел. let num1 = +prompt("enter num 1") let num2 = +prompt("enter num 2") let num3 = +prompt("enter num 3") 
+let nums = [100, 500] // 700, 1000, 500 --> [100, 500, 700, 1000, 500] nums.push(num1, num2, num3) console.log(nums); */
+
+
+let nums = []
+for (let i = 0; i < 3; i++){
+num1 = +prompt("Введите число")
+nums.push(num1)
+}
+console.log(nums)
